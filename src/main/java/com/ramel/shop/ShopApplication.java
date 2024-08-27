5b1a671534ebb7380696abe496b079f29bfa1e46
Ramel <dev_20@naver.com>
2024-08-27 18:06:51 +0900

    db세팅

--- src/main/java/com/ramel/shop/ShopApplication.java
+++ src/main/java/com/ramel/shop/ShopApplication.java
@@ -9,13 +9,4 @@ public class ShopApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
     }
-
-    class Friend {
-        String name = "kim";
-        int age = 20;
-
-        Friend(String a) {
-            this.name = a;
-        }
-    }
 }
