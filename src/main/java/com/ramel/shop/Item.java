11afb1d3546685c9774485d29738780961e8061b
Ramel <dev_20@naver.com>
2024-08-29 20:14:13 +0900

    JPA entity

--- src/main/java/com/ramel/shop/Item.java
+++ src/main/java/com/ramel/shop/Item.java
@@ -2,11 +2,13 @@ package com.ramel.shop;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
 @ToString
 @Getter
+@NoArgsConstructor
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,4 +19,10 @@ public class Item {
 
     @Column
     private String title;
+
+    public Item(Long id, Integer price, String title) {
+        this.id = id;
+        this.price = price;
+        this.title = title;
+    }
 }
--- src/main/java/com/ramel/shop/ItemController.java
+++ src/main/java/com/ramel/shop/ItemController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -20,5 +21,21 @@ public class ItemController {
         model.addAttribute("items", result);
         return "list.html";
     }
+    
+    /*
+    상품추가 기능?
+    1. 상품 이름, 가격 작성할 수 있는 페이지와 폼
+    2. 전송버튼 누르면 서버로 보냄
+    3. 서버는 검사 후 이상없으면 DB 저장
+     */
 
+    @GetMapping("/write")
+    String write() {
+        return "write.html";
+    }
+
+    @PostMapping("/add")
+    String write(String title, String price) {
+        return "redirect:/list";
+    }
 }
