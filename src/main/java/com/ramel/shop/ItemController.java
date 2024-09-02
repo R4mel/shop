0ace86c10fc617835e0e1655e0aff82b308a8136
Ramel <dev_20@naver.com>
2024-09-02 21:42:52 +0900

    에러 처리

--- src/main/java/com/ramel/shop/ItemController.java
+++ src/main/java/com/ramel/shop/ItemController.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -41,4 +43,27 @@ public class ItemController {
         itemRepository.save(item);
         return "redirect:/list";
     }
+
+    @GetMapping("/detail/{id}")
+    String detail(@PathVariable Long id, Model model) {
+        // throw new Exception();
+        try {
+            Optional<Item> result = itemRepository.findById(id);
+            if (result.isPresent()) {
+                model.addAttribute("item", result.get());
+                return "detail.html";
+            } else {
+                return "redirect:/list";
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return "redirect:/list";
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("에러에러");
+        }
+    }
+
+//    @ExceptionHandler(Exception.class) // 모든 API의 에러 캐치
+//    public void handler() {
+//        return ResponseEntity.status().body();
+//    }
 }
