7ed1f4d9e1faeafd3d5eb3cee9dbf7425e0f2434
Ramel <dev_20@naver.com>
2024-09-03 12:55:20 +0900

    ExceptionHandler

--- src/main/java/com/ramel/shop/MyExceptionHandler.java
+++ src/main/java/com/ramel/shop/MyExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice // 모든 Controller 파일의 에러 캐치
 public class MyExceptionHandler { // 컨트롤러마다 Exception 코드를 넣는게 귀찮을 때 사용
-    @ExceptionHandler(Exception.class) // 모든 API의 에러 캐치
+    @ExceptionHandler(Exception.class) // 모든 API 에러 캐치
     public ResponseEntity<String> handler() {
         return ResponseEntity.status(400).body("에러에러");
     }
