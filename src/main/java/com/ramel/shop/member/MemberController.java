4bb6ceb049bd5c3c21f1a4c154c87fcfafa50cb5
Ramel <dev_20@naver.com>
2024-09-13 15:36:13 +0900

    DTO

--- src/main/java/com/ramel/shop/member/MemberController.java
+++ src/main/java/com/ramel/shop/member/MemberController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
 @Controller
@@ -43,4 +44,31 @@ public class MemberController {
         System.out.println(authentication.getName());
         return "mypage.html";
     }
+
+    /*
+    1. Map 자료에 필요한 것만 넣어서 보내기
+    2.
+     */
+    @GetMapping("/user/1")
+    @ResponseBody
+    public MemberDto getUser() {
+        var a = memberRepository.findById(1L);
+        return new MemberDto(a.get().getUsername(), a.get().getDisplayName());
+    }
 }
+
+class MemberDto { // Data Transfer Object: 데이터 변환용 클래스
+    public String username;
+    public String displayName;
+
+    MemberDto(String a, String b) {
+        this.username = a;
+        this.displayName = b;
+    }
+}
+
+/*
+    DTO의 장점?
+    1. 보내는 데이터의 타입체크가 쉬움
+    2. 재사용이 쉬움
+ */
